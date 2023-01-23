3e660f70469aac1913f00e00c4d2978dbba64969
Wojnar <mateusz@sntx.pl>
2023-01-23 19:33:17 +0100

    Deps bump

--- mix.exs
+++ mix.exs
@@ -34,48 +34,48 @@ defmodule Sntx.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.4"},
+      {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
-      {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:cors_plug, "~> 2.0"},
-      {:hackney, "~> 1.8"},
+      {:gettext, "~> 0.22"},
+      {:jason, "~> 1.4"},
+      {:plug_cowboy, "~> 2.6"},
+      {:cors_plug, "~> 3.0"},
+      {:hackney, "~> 1.18"},
 
       # Database
-      {:ecto_sql, "~> 3.7"},
+      {:ecto_sql, "~> 3.9"},
       {:ecto_enum, "~> 1.4"},
       {:ecto_psql_extras, ">= 0.0.0"},
       {:postgrex, ">= 0.0.0"},
 
       # Auth
-      {:argon2_elixir, "~> 2.4.0"},
-      {:guardian, "~> 2.2"},
+      {:argon2_elixir, "~> 3.0.0"},
+      {:guardian, "~> 2.3"},
       {:guardian_db, "~> 2.1"},
 
       # Graph
-      {:absinthe, "~> 1.6.7"},
+      {:absinthe, "~> 1.7"},
       {:absinthe_phoenix, "~> 2.0"},
       {:absinthe_plug, "~> 1.5"},
       {:dataloader, "~> 1.0"},
       {:absinthe_error_payload, "~> 1.1"},
 
       # Dashboard
-      {:phoenix_live_dashboard, "~> 0.6"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:telemetry, "~> 0.4"},
+      {:phoenix_live_dashboard, "~> 0.7"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
+      {:telemetry, "~> 1.2"},
 
       # Deployment
-      {:edeliver, "~> 1.8"},
+      {:edeliver, "~> 1.9"},
       {:distillery, "~> 2.1"},
 
       # Libs
       {:bamboo, "~> 2.2"},
-      {:bamboo_smtp, "~> 4.1"},
+      {:bamboo_smtp, "~> 4.2"},
       {:file_info, ">= 0.0.0"},
-      {:nimble_csv, "~> 1.1"},
-      {:paginator, "~> 1.0"},
+      {:nimble_csv, "~> 1.2"},
+      {:paginator, "~> 1.2"},
       {:recase, "~> 0.7"},
       {:secure_random, "~> 0.5"},
       {:sentry, "~> 8.0"},
@@ -86,10 +86,10 @@ defmodule Sntx.MixProject do
       # Dev
       {:sobelow, "~> 0.11", only: :dev},
       {:ex_machina, "~> 2.7"},
-      {:faker, "~> 0.16", only: :test},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
-      {:phoenix_live_reload, "~> 1.3", only: :dev}
+      {:faker, "~> 0.17", only: :test},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
+      {:phoenix_live_reload, "~> 1.4", only: :dev}
     ]
   end
 
